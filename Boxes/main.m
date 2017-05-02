8632809ac5077f5d256cfc1c3ea04de39d4f6878
Marc Maguire <maguire.marc@gmail.com>
2017-05-02 11:15:45 -0400

    final commit

--- Boxes/main.m
+++ Boxes/main.m
@@ -7,11 +7,24 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Box.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        Box *firstBox = [[Box alloc]initWithHeight:10 width:10 length:5];
+        NSLog(@"The volume of firstBox is %f",[firstBox calculateVolume]);
+        Box *secondBox = [[Box alloc]initWithHeight:2 width:2 length:2];
+        NSLog(@"You can fit %f boxes of secondBox into firstBox",[firstBox howManyBoxesCanFitInMe:secondBox]);
+        
+    
+        
+        
+        
+        
+        
+        
+        
     }
     return 0;
 }
